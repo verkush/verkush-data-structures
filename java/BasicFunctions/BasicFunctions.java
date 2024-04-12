bbdaf24fcf303e4259cf0a8b1fee271af75f1561
Ayush Kumar Verma <ayushkumarverma97@outlook.com>
2024-04-13 00:53:37 +0530

    Add basic java function

---
+++ java/BasicFunctions/BasicFunctions.java
@@ -0,0 +1,10 @@
+public class BasicFunctions {
+
+	public static void printHelloWorld() {
+		System.out.println("Hello, World!");
+	}
+
+	public static void main(String args[]) {
+		printHelloWorld();
+	}
+}
